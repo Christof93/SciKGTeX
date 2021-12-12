d8c8713c0a21084c8b963fbdb0d55f88e917e25b
Christof93 <christofbless@gmail.com>
2021-12-12 23:47:19 +0100

    add tests and test suite

--- test/build.sh
+++ test/build.sh
@@ -2,4 +2,11 @@ lualatex orkg4latex.dtx
 cp orkg4latex.sty test
 cp orkg4latex.lua test
 cd test
-lualatex CrowdRE-VideoComments_v03_camera-ready.tex
+#lualatex CrowdRE-VideoComments_v03_camera-ready.tex
+
+for folder in test*; do
+    echo "\n---------------------------- Executing tests in folder ${folder} -----------------------------------.\n" 
+    lualatex ${folder}/test.tex
+    mv test.* ${folder}
+    mv xmp_metadata.xml ${folder}
+done
