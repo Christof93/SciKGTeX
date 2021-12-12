7325b789934c26ba03fd42d2ac9ca291caaa9f39
Christof93 <christofbless@gmail.com>
2021-12-13 00:16:19 +0100

    add some actual testing against expected xmp output

--- test/build.sh
+++ test/build.sh
@@ -2,11 +2,13 @@ lualatex orkg4latex.dtx
 cp orkg4latex.sty test
 cp orkg4latex.lua test
 cd test
-#lualatex CrowdRE-VideoComments_v03_camera-ready.tex
 
 for folder in test*; do
     echo "\n---------------------------- Executing tests in folder ${folder} -----------------------------------.\n" 
     lualatex ${folder}/test.tex
     mv test.* ${folder}
     mv xmp_metadata.xml ${folder}
+    cmp --silent ${folder}/xmp_metadata.xml ${folder}/xmp_metadata_expected.xml && 
+    echo "\n\033[0;32m----------------------------### PASS: XMP as expected! ###----------------------------------------\033[0m\n" || 
+    echo "\n\033[0;31m----------------------------### FAIL: XMP not as expected! ###------------------------------------\033[0m\n" 
 done
