8cde9090dfb08a6a331f005d56adcefe6830cb51
Christof93 <christofbless@gmail.com>
2021-12-13 22:52:22 +0100

    add new tests and correct expectations

--- test/build.sh
+++ test/build.sh
@@ -5,10 +5,10 @@ cd test
 
 for folder in test*; do
     echo "\n---------------------------- Executing tests in folder ${folder} -----------------------------------.\n" 
-    lualatex ${folder}/test.tex
+    lualatex --interaction=batchmode ${folder}/test.tex
     mv test.* ${folder}
     mv xmp_metadata.xml ${folder}
     cmp --silent ${folder}/xmp_metadata.xml ${folder}/xmp_metadata_expected.xml && 
-    echo "\n\033[0;32m----------------------------### PASS: XMP as expected! ###----------------------------------------\033[0m\n" || 
-    echo "\n\033[0;31m----------------------------### FAIL: XMP not as expected! ###------------------------------------\033[0m\n" 
+    echo "\n\033[0;32m----------------------------### ${folder} PASS: XMP as expected! ###----------------------------------------\033[0m\n" || 
+    echo "\n\033[0;31m----------------------------### ${folder} FAIL: XMP not as expected! ###------------------------------------\033[0m\n" 
 done
