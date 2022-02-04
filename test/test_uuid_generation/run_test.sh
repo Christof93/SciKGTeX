7c776e52f06bd7c9ae4a8694e4f02362b4a04e3d
Christof93 <christofbless@gmail.com>
2022-02-04 10:36:50 +0100

    debug the uuid test

--- test/test_uuid_generation/run_test.sh
+++ test/test_uuid_generation/run_test.sh
@@ -5,15 +5,11 @@ mv test.aux $1
 mv test.log $1
 mv test.pdf $1
 mv test.xmp_metadata.xml $1
-
-if [ "$x" = "valid" ]; then
-    :
-else
-    printf "\n\033[0;31m----------------------------### uuid generation test FAIL: No new UUID generated! ###------------------------------------\033[0m\n"
-    exit 0
-fi
-
-if cmp --silent $1/test.xmp_metadata.xml $1/xmp_metadata_expected.xml; then
+echo "$(head -1 $1/test.xmp_metadata.xml)"
+echo "$(head -1 $1/xmp_metadata_expected.xml)"
+# the headers are not equal
+if [[ "$(head -1 $1/test.xmp_metadata.xml)" != "$(head -1 $1/xmp_metadata_expected.xml)" ]]; then
     printf "\n\033[0;32m----------------------------### uuid generation test PASS: New UUID generated! ###----------------------------------------\033[0m\n"
 else
-    printf "\n\033[0;31m----------------------------### uuid generation test FAIL: XMP not as expected! ###------------------------------------\033[0m\n"
+    printf "\n\033[0;31m----------------------------### uuid generation test FAIL: No new UUID generated! ###------------------------------------\033[0m\n"
+fi
