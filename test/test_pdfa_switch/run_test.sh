f882fef4e48ec22a3f9c87a6f5e8cdef72e186fe
Christof93 <christofbless@gmail.com>
2023-02-11 01:01:51 +0100

    add tests for pdfa

---
+++ test/test_pdfa_switch/run_test.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+printf "\n---------------------------- Executing uncompressed metadata test -----------------------------------.\n" 
+echo '<?xpacket begin="?" id="48fdc517-5814-4d0c-cd03-0c296941c6"?>\n' > test.xmp_metadata.xml
+lualatex --interaction=batchmode $1/test.tex
+mv test.aux $1
+mv test.log $1
+mv test.pdf $1
+mv test.xmp_metadata.xml $1
+grep SciKGMetadata $1/test.pdf && 
+printf "\n\033[0;32m----------------------------### uncompressed metadata test PASS: custom SciKGMetadata entry found in catalog! ###----------------------------------------\033[0m\n" || 
+printf "\n\033[0;31m----------------------------### uncompressed metadata test FAIL: custom SciKGMetadata entry not found in catalog! ###------------------------------------\033[0m\n"
